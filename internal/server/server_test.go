94a8dd1328571c46ad8c87019302cb30a383ae9d
vasconcelosdavi <davi.muniz41@gmail.com>
2022-11-27 19:42:58 -0300

    Test GetIdFromLocal

--- internal/server/server_test.go
+++ internal/server/server_test.go
@@ -7,9 +7,12 @@ import (
 	. "github.com/onsi/gomega"
 	"time"
 	"fmt"
+	"strconv"
 )
 
 var _ = Describe("Server", func() {
+	var clientId int
+
 	When("A new user open the client", func() {
 		It(`Should be able to register its 
 		    information in the raw ZooKeeper server`, func() {
@@ -22,9 +25,19 @@ var _ = Describe("Server", func() {
 			publicKey := "i1RfARNCYn9+K3xmRNTaXG9sVSK6TMgY9l8SDm3MUZ4="
 			path, err := RegisterUser(conn, username, ipv4, publicKey)
 			usersData, _ := zookeeper.GetZNode(conn, usersPath)
+			clientId, _ = strconv.Atoi(usersData)
 			expectedPath := fmt.Sprintf("%s/id%s", usersPath, usersData)
 			Expect(err).To(BeNil())
 			Expect(path).To(Equal(expectedPath))
+		})
+	})
+
+	When("A old user connects", func() {
+		It(`Should be able to get its id
+		    from the local storage`, func() {
+			id, idError := GetIdFromLocal()
+			Expect(idError).To(BeNil())
+			Expect(id).To(Equal(clientId))
 		})
 	})
 })
