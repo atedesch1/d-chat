0462cc816b0e764da87664244e45317bc58cd132
atedesch1 <alvarotedeschi@gmail.com>
2022-11-27 18:15:31 -0300

    simple client

---
+++ internal/chat/channel.go
@@ -0,0 +1,27 @@
+package chat
+
+import (
+	chat_message "github.com/decentralized-chat/pb"
+	"google.golang.org/grpc"
+)
+
+type Channel struct {
+	id    uint
+	users []chat_message.User
+}
+
+type Peer struct {
+	user   chat_message.User
+	client chat_message.ChatServiceClient
+	conn   *grpc.ClientConn
+}
+
+func NewChannel(id uint) *Channel {
+	return &Channel{
+		id: id,
+	}
+}
+
+func (c *Channel) AddUser(user chat_message.User) {
+	c.users = append(c.users, user)
+}
---
+++ internal/chat/client.go
@@ -0,0 +1,88 @@
+package chat
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"time"
+
+	chat_message "github.com/decentralized-chat/pb"
+	"google.golang.org/grpc"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+type Client struct {
+	chat_message.UnimplementedChatServiceServer
+
+	User chat_message.User
+
+	lis net.Listener
+	srv *grpc.Server
+
+	peers []Peer
+}
+
+func NewClient(username string, port uint) *Client {
+	return &Client{
+		User: chat_message.User{
+			Username: username,
+			Addr: &chat_message.Address{
+				Ip:   "localhost",
+				Port: uint32(port),
+			},
+		},
+	}
+}
+
+func (c *Client) SendMessage(ctx context.Context, msg *chat_message.ContentMessage) (*chat_message.AckMessage, error) {
+	log.Println("Received message from", msg.From, ". Content:", msg.Content)
+
+	return &chat_message.AckMessage{
+		From:   &c.User,
+		SentAt: timestamppb.Now(),
+	}, nil
+}
+
+func (c *Client) BroadcastMessage(content string) error {
+	msg := &chat_message.ContentMessage{
+		From:    &c.User,
+		Content: content,
+		SentAt:  timestamppb.Now(),
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	for _, peer := range c.peers {
+		ack, err := peer.client.SendMessage(ctx, msg)
+		if err != nil {
+			return err
+		}
+		log.Println("Ack from", ack.From)
+	}
+
+	return nil
+}
+
+func (c *Client) ListenForInput() {
+	inputChannel := make(chan string)
+
+	// Read input
+	go func() {
+		reader := bufio.NewReader(os.Stdin)
+		for {
+			input, _, err := reader.ReadLine()
+			if err != nil {
+				fmt.Println("Error:", err.Error())
+			}
+			inputChannel <- string(input)
+		}
+	}()
+
+	for input := range inputChannel {
+		go c.BroadcastMessage(input)
+	}
+}
