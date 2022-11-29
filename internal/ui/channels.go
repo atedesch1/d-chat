442af1dfa6a889e5395c4e86e6c18ab56f93e15a
atedesch1 <alvarotedeschi@gmail.com>
2022-11-28 22:08:43 -0300

    add create channel input

--- internal/ui/channels.go
+++ internal/ui/channels.go
@@ -3,20 +3,42 @@ package ui
 import (
 	"fmt"
 
+	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
 )
 
+const (
+	listView      view = "listView"
+	textInputView view = "textInputView"
+)
+
 type channelsModel struct {
+	focusedView view
+
 	channels []string
 	cursor   int
 	loading  bool
+
+	textInput   textinput.Model
+	channelName string
 }
 
 func newChannelsModel() *channelsModel {
+	ti := textinput.New()
+	ti.Placeholder = "Channel"
+	ti.Prompt = ""
+	ti.CharLimit = 16
+	ti.Width = 20
+
 	return &channelsModel{
+		focusedView: listView,
+
 		channels: []string{},
 		cursor:   0,
 		loading:  true,
+
+		textInput:   ti,
+		channelName: "",
 	}
 }
 
@@ -34,13 +56,19 @@ func (m *channelsModel) chooseChannelCmd() tea.Msg {
 	return ChannelChosenMsg(m.channels[m.cursor])
 }
 
+type channelCreatedMsg string
+
+func (m *channelsModel) createChannelCmd() tea.Msg {
+	return channelCreatedMsg(m.channelName)
+}
+
 func (m *channelsModel) Init() tea.Cmd {
 	return m.getChannelsCmd
 }
 
 func (m *channelsModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	var (
-		// cmd  tea.Cmd
+		cmd  tea.Cmd
 		cmds []tea.Cmd
 	)
 
@@ -50,21 +78,51 @@ func (m *channelsModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		m.loading = false
 		m.channels = msg
 
+	case channelCreatedMsg:
+		cmds = append(cmds, m.createChannelCmd)
+
 	case tea.KeyMsg:
 		switch msg.Type {
+		case tea.KeyTab:
+			switch m.focusedView {
+			case listView:
+				m.focusedView = textInputView
+			case textInputView:
+				m.focusedView = listView
+			}
+
+		case tea.KeyCtrlR:
+			m.cursor = 0
+			cmds = append(cmds, m.getChannelsCmd)
+
 		case tea.KeyUp:
-			if m.cursor > 0 {
+			if m.focusedView == listView && m.cursor > 0 {
 				m.cursor--
 			}
 
 		case tea.KeyDown:
-			if m.cursor+1 < len(m.channels) {
+			if m.focusedView == listView && m.cursor+1 < len(m.channels) {
 				m.cursor++
 			}
 
 		case tea.KeyEnter:
-			cmds = append(cmds, m.chooseChannelCmd)
+			switch m.focusedView {
+			case listView:
+				cmds = append(cmds, m.chooseChannelCmd)
+			case textInputView:
+				m.channelName = m.textInput.Value()
+				m.textInput.Reset()
+				cmds = append(cmds, m.createChannelCmd)
+			}
 		}
+	}
+
+	switch m.focusedView {
+	case listView:
+
+	case textInputView:
+		m.textInput, cmd = m.textInput.Update(msg)
+		cmds = append(cmds, cmd)
 	}
 
 	return m, tea.Batch(cmds...)
@@ -78,12 +136,18 @@ func (m *channelsModel) View() string {
 	} else {
 		for i := 0; i < len(m.channels); i++ {
 			cursor := " "
-			if m.cursor == i {
+			if m.cursor == i && m.focusedView == listView {
 				cursor = ">"
 			}
 			channel := m.channels[i]
 			s += fmt.Sprintf("%s %s\n", cursor, channel)
 		}
+
+		cursor := " "
+		if m.focusedView == textInputView {
+			cursor = ">"
+		}
+		s += fmt.Sprintf("\n\nCreate Channel:\n%s %s\n", cursor, m.textInput.View())
 	}
 	return s
 }
