c0fb7802e8469a2c996b2a2df9b2f31dba79a49a
vasconcelosdavi <davi.muniz41@gmail.com>
2022-11-28 21:10:31 -0300

    Fix errors

--- internal/server/server.go
+++ internal/server/server.go
@@ -6,11 +6,9 @@ import (
 	"strconv"
 	"log"
 	"fmt"
-	"os"
-	"bufio"
-	"path/filepath"
 	"time"
 	"strings"
+	"errors"
 )
 
 const (
@@ -53,16 +51,53 @@ func (s *Server) Init(ipv4 string, port string) error {
 	s.conn = conn
 	if err != nil {
 		log.Fatal(err)
-		return error.New("Error when connecting to ZooKeeper.")
+		return errors.New("Error when connecting to ZooKeeper.")
 	}
 	return nil
+}
+
+func ParseUserData(data string) *UserInfo {
+	lines := strings.Split(data, "\n")
+	username := strings.Split(lines[0], " ")[1]
+	ipv4 := strings.Split(lines[1], " ")[1]
+	port := strings.Split(lines[2], " ")[1]
+	publicKey := strings.Split(lines[3], " ")[1]
+	ui := new(UserInfo)
+	ui.Init(username, ipv4, port, publicKey)
+	return ui
+}
+
+func ParseChannelData(data string) *ChannelInfo {
+	temp := strings.Split(data, "\n")
+	channelname := strings.Split(temp[0], " ")[1]
+	users := strings.Split(temp[1], " ")[1:]
+	ci := new(ChannelInfo)
+	ci.Init(channelname, users)
+	return ci
+}
+
+func (s *Server) GetUserIdFromUsername(user string) (int, error) {
+	children, _, err := s.conn.Children(usersPath)
+	if err != nil {
+		log.Fatal(err)
+		return -1, errors.New("error when accessing /users children")
+	}
+	for _, userId := range children {
+		data, _ := zookeeper.GetZNode(s.conn, fmt.Sprintf("%s/%s", usersPath, userId))
+		ui := ParseUserData(data)
+		if ui.username == user {
+			userIdConverted, _ := strconv.Atoi(userId[2:])
+			return userIdConverted, nil
+		}
+	}
+	return -1, errors.New("username not found")
 }
 
 func (s *Server) RegisterUser(user string, ipv4 string, port string, publicKey string) error {
 	usersExists := zookeeper.CheckZNode(s.conn, usersPath)
 	if usersExists == false {
 		log.Fatalf("You must set %s path in the ZooKeeper.", usersPath)
-		return error.New("no path /users in zookeeper")
+		return errors.New("no path /users in zookeeper")
 	}
 
 	numberOfUsersString, version := zookeeper.GetZNode(s.conn, usersPath)
@@ -73,28 +108,28 @@ func (s *Server) RegisterUser(user string, ipv4 string, port string, publicKey s
 	userPath := fmt.Sprintf("%s/id%d", usersPath, numberOfUsersUpdated)
 	userData := fmt.Sprintf("username %s\nipv4 %s\nport %s\npublic-key %s", user, ipv4, publicKey)
 	flagPermanent := int32(0)
-	ZNodePath, err := zookeeper.CreateZNode(s.conn, userPath, flagPermanent, userData)
+	_, err := zookeeper.CreateZNode(s.conn, userPath, flagPermanent, userData)
 	return err
 }
 
 func (s *Server) SetUserOnline(user string) error {
-	userId, err := GetUserIdFromUsername(user)
+	userId, err := s.GetUserIdFromUsername(user)
 	if err != nil {
 		log.Fatal(err)
 	}
 	connExists := zookeeper.CheckZNode(s.conn, connPath)
 	if connExists == false {
 		log.Fatalf("You must set %s path in the ZooKeeper.", connPath)
-		return error.New("no path /conn in zookeeper")
+		return errors.New("no path /conn in zookeeper")
 	}
 
 	userConnPath := fmt.Sprintf("%s/id%d", connPath, userId)
-	ZNodeConnPath, err := zookeeper.CreateZNode(s.conn, userConnPath, zk.FlagEphemeral, "")
+	_, err = zookeeper.CreateZNode(s.conn, userConnPath, zk.FlagEphemeral, "")
 	return err
 }
 
 func (s *Server) IsUserRegistered(user string) bool {
-	userId, err := GetUserIdFromUsername(user)
+	userId, err := s.GetUserIdFromUsername(user)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -113,20 +148,21 @@ func (s *Server) IsUserInsideChannel(channelname string, user string) bool {
 }
 
 func (s *Server) IsUserOnline(user string) (bool, error) {
-	userExists := IsUserRegistered(user)
+	userExists := s.IsUserRegistered(user)
 	if userExists == false {
-		return false, error.New("user was not registered")
+		return false, errors.New("user was not registered")
 	}
 	children, _, err := s.conn.Children(connPath)
 	if err != nil {
 		log.Fatal(err)
 	}
-	userId, err := GetUserIdFromUsername(user)
+	userId, err := s.GetUserIdFromUsername(user)
 	if err != nil {
 		log.Fatal(err)
 	}
 	for _, id := range children {
-		if userId == strconv.Atoi(id[2:]) {
+		userIdInt, _ := strconv.Atoi(id[2:])
+		if userId == userIdInt {
 			return true, nil
 		}
 	}
@@ -134,14 +170,14 @@ func (s *Server) IsUserOnline(user string) (bool, error) {
 }
 
 func (s *Server) RegisterChannel(channelName string, user string) error {
-	userId, err := GetUserIdFromUsername(user)
+	userId, err := s.GetUserIdFromUsername(user)
 	if err != nil {
 		log.Fatal(err)
 	}
 	channelsExists := zookeeper.CheckZNode(s.conn, channelsPath)
 	if channelsExists == false {
 		log.Fatalf("You must set %s path in the ZooKeeper.", channelsPath)
-		return error.New("no path /channels in zookeeper")
+		return errors.New("no path /channels in zookeeper")
 	}
 
 	numberOfChannelsString, version := zookeeper.GetZNode(s.conn, channelsPath)
@@ -152,14 +188,14 @@ func (s *Server) RegisterChannel(channelName string, user string) error {
 	channelPath := fmt.Sprintf("%s/ch%d", channelsPath, numberOfChannelsUpdated)
 	channelData := fmt.Sprintf("channelname %s\nusers id%d", channelName, userId)
 	flagPermanent := int32(0)
-	ZNodePath, err := zookeeper.CreateZNode(s.conn, channelPath, flagPermanent, channelData)
+	_, err = zookeeper.CreateZNode(s.conn, channelPath, flagPermanent, channelData)
 	return err
 }
 
 func (s *Server) AddUserToChannel(channelName string, user string) error {
-	userInsideChannel := IsUserInsideChannel(channelname, user)
+	userInsideChannel := s.IsUserInsideChannel(channelName, user)
 	if userInsideChannel == true {
-		return error.New("user is already in the channel")
+		return errors.New("user is already in the channel")
 	}
 	children, _, err := s.conn.Children(channelsPath)
 	if err != nil {
@@ -167,15 +203,15 @@ func (s *Server) AddUserToChannel(channelName string, user string) error {
 	}
 	for _, channelId := range children {
 		data, version := zookeeper.GetZNode(s.conn, fmt.Sprintf("%s/%s", channelsPath, channelId))
-		currChannelName, channelData := ParseChannelData(data)
-		if currChannelName == channelName {
-			channelData = append(channelData, fmt.Sprintf(" %s\n", user))
-			channelDataStr := GenerateChannelData(currChannelName, channelData)
+		ci := ParseChannelData(data)
+		if ci.channelname == channelName {
+			ci.users = append(ci.users, fmt.Sprintf(" %s\n", user))
+			channelDataStr := GenerateChannelData(ci.channelname, ci.users)
 			zookeeper.SetZNode(s.conn, fmt.Sprintf("%s/%s", channelsPath, channelId), channelDataStr, version)
 			return nil
 		}
 	}
-	return error.New("channel does not exist")
+	return errors.New("channel does not exist")
 }
 
 func (s *Server) GetChannelUsers(channelname string) []string {
@@ -185,12 +221,12 @@ func (s *Server) GetChannelUsers(channelname string) []string {
 	}
 	for _, channelId := range children {
 		data, _ := zookeeper.GetZNode(s.conn, fmt.Sprintf("%s/%s", channelsPath, channelId))
-		currChannelName, channelData := ParseChannelData(data)
-		if currChannelName == channelname {
-			return channelData
+		ci := ParseChannelData(data)
+		if ci.channelname == channelname {
+			return ci.users
 		}
 	}
-	return []string
+	return []string{}
 }
 
 func (s *Server) GetChannelsName() []string {
@@ -201,8 +237,8 @@ func (s *Server) GetChannelsName() []string {
 	}
 	for _, channelId := range children {
 		data, _ := zookeeper.GetZNode(s.conn, fmt.Sprintf("%s/%s", channelsPath, channelId))
-		currChannelName, _ := ParseChannelData(data)
-		channels = append(channels, currChannelName)		
+		ci := ParseChannelData(data)
+		channels = append(channels, ci.channelname)		
 	}
 	return channels
 }
@@ -214,14 +250,14 @@ func (s *Server) DeleteChannel(channelname string) error {
 	}
 	for _, channelId := range children {
 		data, version := zookeeper.GetZNode(s.conn, fmt.Sprintf("%s/%s", channelsPath, channelId))
-		currChannelName, _ := ParseChannelData(data)
-		if currChannelName == channelname {
+		ci := ParseChannelData(data)
+		if ci.channelname == channelname {
 			deletePath := fmt.Sprintf("%s/%s", channelsPath, channelId)
 			zookeeper.DeleteZNode(s.conn, deletePath, version)
 			return nil
 		}
 	}
-	return error.New("cannot delete a channel that does not exist")
+	return errors.New("cannot delete a channel that does not exist")
 }
 
 func (s *Server) DeleteUserFromChannel(channelname string, user string) error { 
@@ -231,11 +267,10 @@ func (s *Server) DeleteUserFromChannel(channelname string, user string) error {
 	}
 	for _, channelId := range children {
 		data, version := zookeeper.GetZNode(s.conn, fmt.Sprintf("%s/%s", channelsPath, channelId))
-	 	currChannelName, currUsers := ParseChannelData(data)
-	 	if currChannelName == channelname {
-			userId := s.GetUserIdFromUsername(user)
-			newData := fmt.Sprintf("channelname %s\nusers", currChannelName)
-			for _, currUser := range currUsers {
+	 	ci := ParseChannelData(data)
+	 	if ci.channelname == channelname {
+			newData := fmt.Sprintf("channelname %s\nusers", ci.channelname)
+			for _, currUser := range ci.users {
 				if currUser == user {
 					continue
 				}
@@ -245,24 +280,7 @@ func (s *Server) DeleteUserFromChannel(channelname string, user string) error {
 			return nil
 		}
 	}
-	return error.New("cannot delete a user that is not in the channel")
-}
-
-func (s *Server) GetUserIdFromUsername(user string) (int, error) {
-	children, _, err := s.conn.Children(usersPath)
-	if err != nil {
-		log.Fatal(err)
-		return -1, error.New("error when accessing /users children")
-	}
-	for _, userId := range children {
-		data, _ := zookeeper.GetZNode(s.conn, fmt.Sprintf("%s/%s", usersPath, userId))
-		username, _, _ := ParseUserData(data)
-		if username == user {
-			userIdConverted, _ := strconv.Atoi(userId[2:])
-			return userIdConverted, nil
-		}
-	}
-	return -1, error.New("username not found")
+	return errors.New("cannot delete a user that is not in the channel")
 }
 
 func GenerateChannelData(channelname string, users []string) string {
@@ -272,24 +290,4 @@ func GenerateChannelData(channelname string, users []string) string {
 		data += formatUsername
 	}
 	return data
-}
-
-func ParseUserData(data string) UserInfo {
-	lines := strings.Split(data, "\n")
-	username := strings.Split(lines[0], " ")[1]
-	ipv4 := strings.Split(lines[1], " ")[1]
-	port := strings.Split(lines[2], " ")[1]
-	publicKey := strings.Split(lines[3], " ")[1]
-	ui := New(UserInfo)
-	ui.Init(username, ipv4, port, publicKey)
-	return ui
-}
-
-func ParseChannelData(data string) ChannelInfo {
-	temp := strings.Split(data, "\n")
-	channelname := strings.Split(temp[0], " ")[1]
-	users := strings.Split(tempo[1], " ")[1:]
-	ci := new(ChannelInfo)
-	ci.Init(channelname, users)
-	return ci
 }
