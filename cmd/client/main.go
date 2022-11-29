9723e2528fea00a70e5c983967404775477d1a2b
atedesch1 <alvarotedeschi@gmail.com>
2022-11-29 05:41:09 -0300

    flush screen after register

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"flag"
+	"fmt"
 
+	tm "github.com/buger/goterm"
 	"github.com/decentralized-chat/internal/chat"
 )
 
@@ -20,6 +22,12 @@ func main() {
 	c.RegisterUser()
 
 	c.RegisterServer()
+
+	tm.Clear()
+	tm.MoveCursor(1, 1)
+	tm.Flush()
+
+	fmt.Println("Decentralized-Chat")
 
 	go c.ListenForConnections()
 
--- go.mod
+++ go.mod
@@ -11,6 +11,7 @@ require (
 )
 
 require (
+	github.com/buger/goterm v1.0.4
 	github.com/fsnotify/fsnotify v1.4.9 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/go-cmp v0.5.9 // indirect
